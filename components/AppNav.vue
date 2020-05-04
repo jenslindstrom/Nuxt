de2594adb4346f47dbfe47336f75d071241e51f5
jenslindstrom <jens@jgl.se>
2020-05-04 16:13:42 +0200

    Initial commit

---
+++ components/AppNav.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <nuxt-link exact to="/">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link exact to="projects">Projects link</nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        display: flex;
+        justify-content: center;
+        width: 100vw;
+        height: 100%;
+        ul {
+            padding-left: 0;
+            li {
+                display: inline-block;
+            }
+        }
+
+    }
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <Masthead/>
+        <h1>
+            Hello World - Bird is on the fly!
+        </h1>
+        <h2>My projects</h2>
+        <div v-for="project in githubProjects" :key="project.id">
+            <h3> {{ project.name }}</h3>
+            <a :href="project.html_url">{{ project.name }}</a>
+            <p>{{ project.stargazers_count }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+import Masthead from '~/components/Masthead.vue'
+
+export default {
+    computed: {
+        ...mapState(["githubProjects"])
+    },
+    components: {
+        Masthead
+    }
+    /*,
+    mounted() {
+        this.$store.dispatch("getGithubProjects");
+    }*/   
+}
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
